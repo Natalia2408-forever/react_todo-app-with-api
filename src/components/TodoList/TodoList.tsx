9f785765496cc27585b6f8724d04d86d4967a517
Natalia Panchuk <Foreverdanda@ukr.net>
2026-04-01 17:50:47 +0300

    task

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoItem } from '../TodoItem';
+
+type Props = {
+  filteredTodos: Todo[];
+  processings: number[];
+  onDelete: (id: number) => void;
+
+  onUpdate: (todo: Todo) => Promise<void>;
+};
+
+export const TodoList: React.FC<Props> = ({
+  filteredTodos,
+  processings,
+  onDelete,
+
+  onUpdate,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {/* This is a completed todo */}
+      {filteredTodos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          isProcessed={processings.includes(todo.id)}
+          onDelete={onDelete}
+          onUpdate={onUpdate}
+        />
+      ))}
+
+      {/*{tempTodo && (
+        <TodoItem
+          key={tempTodo.id}
+          todo={tempTodo}
+          isProcessed={true}
+          onDelete={() => {}}
+          onUpdate={() => Promise.resolve()}
+        />
+      )}*/}
+    </section>
+  );
+};
---
+++ src/utils/todos.ts
@@ -0,0 +1,25 @@
+import { Todo } from '../types/Todo';
+import { client } from './fetchClient';
+
+/*export const USER_ID = 0;*/
+export const USER_ID = 4081;
+
+export const getTodos = () => {
+  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+};
+
+export function deleteTodo(todoId: number) {
+  return client.delete(`/todos/${todoId}`);
+}
+
+export const addTodo = ({ userId, title, completed }: Omit<Todo, 'id'>) => {
+  return client.post<Todo>(`/todos`, {
+    userId,
+    title,
+    completed,
+  });
+};
+
+export const updateTodo = ({ id, title, completed }: Todo) => {
+  return client.patch<Todo>(`/todos/${id}`, { title, completed });
+};
